45de5ed18c27b981b5be653a45bd8ff96ac0b805
ritikkumar27 <rkv2771@gmail.com>
2026-08-17 15:58:08 +0530

    removed hunting player and store methods from main script

--- pokecatch/core/player.py
+++ pokecatch/core/player.py
@@ -1,5 +1,5 @@
 from pokecatch.config import PLAYER_DATA_FILE, PLAYER_DEX
-from pokecatch.utils import load_data, save_data, display_sprite, display_sprite_pokedex
+from pokecatch.utils import load_data, save_data, display_sprite_pokedex
 
 def load_player_data():
     if not PLAYER_DATA_FILE.exists():
@@ -17,15 +17,14 @@ def load_player_data():
         return player_data
     return load_data(PLAYER_DATA_FILE)
 
-
 def save_player_data(player_data):
     save_data(PLAYER_DATA_FILE, player_data)
-
 
 def inventory():
     player_data = load_player_data()
     currency = player_data.get("currency", 0)
     balls = player_data.get("balls", {})
+
     print("--- 🎒 Your Inventory ---")
     print(f"Poké Dollars: ${currency}")
     print("\n--- Poké Balls ---")
@@ -38,6 +37,7 @@ def my_pokemon():
     if not player_dex:
         print("You haven't caught any Pokémon yet. Go 'hunt' for some!")
         return
+
     print("--- Your Pokémons ---\n")
     sorted_dex = sorted(player_dex, key=lambda p: p['id'])
     for p in sorted_dex:
